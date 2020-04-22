aad2366beb8fbc845715da4521d0ee88ec897070
Fernando <>
2020-04-22 21:24:19 +0700

    Add hal5.php

---
+++ hal5.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+    <head>
+		<meta charset="utf-8">
+		<title>Tentang Pengenbang Web</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    </head>
+		
+		<body>
+
+				<?php include 'navbar.php'; ?>
+
+		<div class="container">
+			<header>
+				<h1>Tentang Pengembang Web</h1>
+			</header>
+			<hr class="featurette-divider">
+      <div class="container marketing">
+        <div class="row">
+          <div class="col-lg-5 text-center mx-auto">
+            <img class="rounded-circle" src="img/fernandoo.JPG" alt="Generic placeholder image" width="140" height="140">
+            <h2>Fernando Hasudungan Manik</h2>
+            <p class="text-justify">Kesehatan merupakan hal yang utama yang harus dimiliki. Karena jika ingin melakukan aktivitas dan kondisi kita tidak fit  makan akan mengganggu kosentrasi kita. Untuk itu saya terinspirasi membuat web kesehatan yang bertujuan untuk mencegah berbagai penyakit. Semoga Bermanfaat.</p>
+						</div>
+					</div>
+			</div>
+			<hr class="featurette-divider">
+
+					<p>Terima Kasih kepada pembaca yang sudah mengunjungi web kesehatan. Jika memiliki pesan atau saran akan diterima dengan baik :)
+					<br>Silahkan kunjungi akun social media yang tertera pada bagian bawah.
+						 <br>Terimakasih kepada setiap pihak dan bagian yang terlibat dalam pembuatan atau si website ini.</p>
+			
+						 <div class="w-30 h-100">
+								<div class="row">
+								</div>
+							</div>
+						</div>
+
+			<?php include 'footer.php'; ?>
+
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+		
+	</div>
+        
+    </body>
+</html>
