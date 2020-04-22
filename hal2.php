6cede592bbda939de47fbaa5927f4b6a2eeabc8a
Fernando <>
2020-04-22 21:22:19 +0700

    Add hal2.php

---
+++ hal2.php
@@ -0,0 +1,91 @@
+
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Tips Mengurangi Berat Badan</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+  </head>
+
+  <body>
+
+			<?php include 'navbar.php'; ?>
+
+    <div class="container">
+      <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+        <div class="col-md-6 px-0">
+          <h1 class="display-4 font-italic">Tips Mengurangi Berat Badan</h1>
+          <p class="lead my-3">Tubuh dengan badan yang melar karena penuh lemak merupakan musah bagi bagi beberapa orang yang ingin berpenampilan ideal. Berikut adalah cara untuk mengurangi berat badan supaya badan kita terlihat ideal.</p>
+        </div>
+			</div>
+    </div>
+
+						<hr class="featurette-divider">
+
+						<div class="row featurette">
+							<div class="col-md-7">
+								<h2 class="featurette-heading">Mengurangi Asupan Kalori</h2>
+								<p class="lead">Mengurangi asupan kalori atau porsi makan terbukti efektif untuk mengurangi berat badan dalam waktu singkat. Kebutuhan kalori setiap orang berbeda-beda, tergantung pada berbagai faktor seperti usia, jenis kelamin, tinggi badan, berat badan, dan aktivitas. Demi memaksimalkan efek diet yang Kamu lakukan, lebih baik kurangi camilan dan bumbu-bumbu penyedap, serta perbanyak konsumsi sayur-sayuran.</p>
+							</div>
+							<div class="col-md-5">
+								<img class="featurette-image img-fluid mx-auto" src="img/mengurangi asupan kalori.jpg" alt="Generic placeholder image">
+							</div>
+						</div>
+		
+						<hr class="featurette-divider">
+		
+						<div class="row featurette">
+							<div class="col-md-7 order-md-2">
+								<h2 class="featurette-heading">Hindari Makanan Siap Saji</h2>
+								<p class="lead">Selain mengurangi asupan kalori, menghindari makanan siap saji juga membantu Kamu menurunkan berat badan. Sebagai gantinya, Kamu bisa memilih makanan yang mengandung banyak protein seperti ikan dan ayam, serta sayuran rendah karbohidrat. Dengan begitu, Kamu tetap bisa merasa kenyang tanpa memakan banyak kalori.</p>
+							</div>
+							<div class="col-md-5 order-md-1">
+								<img class="featurette-image img-fluid mx-auto" src="img/hindari junkfood.jpg" alt="Generic placeholder image">
+							</div>
+						</div>
+		
+						<hr class="featurette-divider">
+		
+						<div class="row featurette">
+								<div class="col-md-7">
+									<h2 class="featurette-heading">Perbanyak olahraga dan aktivitas fisik</h2>
+									<p class="lead">Salah satu cara menurunkan berat badan dalam satu minggu yaitu memperbanyak olahraga dan aktivitas fisik, yang akan membakar kalori. Semakin banyak kalori yang terbakar, berat badan pun akan semakin cepat berkurang. Olahraga seperti mengangkat beban atau senam aerobik bisa Kamu terapkan sehari-hari untuk meningkatkan massa otot dan metabolisme tubuh. Aktivitas ini sekaligus mengurangi endapan karbohidrat dan cairan di dalam tubuh yang membuat berat badan Kamu meningkat.</p>
+								</div>
+								<div class="col-md-5">
+									<img class="featurette-image img-fluid mx-auto" src="img/olahraga terus.jpg" alt="Generic placeholder image">
+								</div>
+							</div>
+		
+						<hr class="featurette-divider">
+
+							<div class="row featurette">
+									<div class="col-md-7 order-md-2">
+										<h2 class="featurette-heading">Kurangi Karbohidrat dan Perbanyak Protein</h2>
+										<p class="lead">Sebagai gantinya, Kamu bisa memperbanyak sayuran rendah karbohidrat, mengonsumsi telur, daging tanpa lemak, serta ikan. Protein juga bisa membantu meningkatkan metabolisme dalam tubuh.</p>
+									</div>
+									<div class="col-md-5 order-md-1">
+										<img class="featurette-image img-fluid mx-auto" src="img/kurangi nasi.jpg" alt="Generic placeholder image">
+									</div>
+								</div>
+				
+								<hr class="featurette-divider">
+
+								<div class="row featurette">
+										<div class="col-md-7">
+											<h2 class="featurette-heading">Rutin Berpuasa</h2>
+											<p class="lead">Manfaat puasa yang dilakukan secara rutin, bisa membantu Kamu menurunkan berat badan. Secara tidak langsung, Kamu akan berusaha membatasi asupan kalori dan makanan lainnya dalam jangka waktu tertentu. Namun beberapa ahli tidak merekomendasikan berpuasa sebagai cara menurunkan berat badan. Kamu juga perlu berhati-hati jika memiliki penyakit lambung atau diabetes, atau mengonsumsi obat-obatan tertentu sebelum mencoba berpuasa untuk menurunkan berat badan. Demi mendapatkan hasil terbaik, Kamu harus menerapkan cara menurunkan berat badan dalam satu minggu secara alami sesuai aturan.</p>
+										</div>
+										<div class="col-md-5">
+											<img class="featurette-image img-fluid mx-auto" src="img/puasa diet.jpg" alt="Generic placeholder image">
+										</div>
+									</div>
+
+									<hr class="featurette-divider">
+
+    <?php include 'footer.php'; ?>
+
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+  </body>
+</html>
