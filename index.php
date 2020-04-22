7b27e662d91abb7ce26fcf4683d0045d953fb46a
Fernando <>
2020-04-22 20:52:51 +0700

    Add index.php file
    This is the main page of this website

---
+++ index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html>
+    <head>
+		<meta charset="utf-8">
+		<title>Web Kesehatan</title>
+		<!-- Bootstrap core CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+		<link rel="stylesheet" href="style.css">
+    </head>
+	
+    <body class="bg-light ">
+		<!-- <h1>Selamat Datang di Website Kesehatan</h1> -->
+		<?php include 'navbar.php'; ?>
+		
+		<h1>Seputar Kesehatan</h1>
+		<div class="container-fluid d-flex flex-row w-100" style="height: 500px">
+			<div class="w-70 h-120">
+				<div class="row">
+					<div  class="col-md-3 text-light img-1">
+						<div class="overlay px-2 py-2">
+							<h2>Cacar Air Menyerang Saat Dewasa, Bagaimana Atasinya?</h2>
+							<p class="text-justify">Cacar air yang menyerang di usia dewasa membuat penderita lebih mudah mengalami komplikasi dibandingkan jika penyakit ini menyerang di saat usia anak-anak. Tapi jangan panik, karena kamu bisa mengatasinya dengan beberapa langkah berikut...</p>
+							<p><a class="btn btn-secondary" href="https://www.vemale.com/kesehatan/114188-cacar-air-menyerang-saat-dewasa-bagaimana-atasinya.html" role="button">View details &raquo;</a></p>
+						</div>
+					</div>
+					<div class="col-md-3 text-light img-2">
+						<div class="overlay py-2 px-2">
+							<h2>Penyebab Bau Kaki dan Tips Mengatasinya</h2>
+							<p class="text-justify">Pernahkah Anda merasa tiba-tiba semua mata tertuju pada Anda, padahal tidak ada yang salah dengan penampilan Anda. Ternyata Anda mencium aroma tidak sedap yang datang dari kaki Anda. Bau kaki memang menjadi salah satu masalah karena cukup mengganggu banyak dan memalukan. Bau pada kaki...</p>
+							<p><a class="btn btn-secondary" href="https://www.webkesehatan.com/penyebab-bau-kaki/" role="button">View details &raquo;</a></p>
+						</div>
+					</div>
+					<div class="col-md-3 text-light img-3">
+						<div class="overlay py-2 px-2">
+							<h2>Keracunan Makanan Karena Kontaminasi dan Cara Mencegahnya</h2>
+							<p class="text-justify">Keracunan makanan merupakan gangguan kesehatan yang bisa secara cepat menjadi endemi. Gejala-gejala keracunan makanan termasuk muntah, mencret, sakit perut, sakit kepala, dan bahkan tak jarang demam. Salah satu penyebab keracunan makanan adalah...</p>
+							<p><a class="btn btn-secondary" href="https://www.webkesehatan.com/keracunan-makanan-karena-kontaminasi-silang/" role="button">View details &raquo;</a></p>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="overlay py-2 px-2">
+							<h2>Tips</h2>
+							<p>Periksakan diri Anda pada dokter untuk mengetahui penyakit yang sedang dialami. Lebih baik mencegah dari pada melindungi</p>
+							<br>
+							<p>Donorkan darah Anda untuk orang yang membutuhkan. Semoga apa yang diberikan dapat bermanfaat.</p> 
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+			
+			<div class="w-30 h-100">
+				<div class="row">
+				</div>
+			</div>
+
+		</div>
+
+		<marquee>"Di dalam tubuh yang sehat terdapat jiwa yang kuat"</marquee>
+		
+		<?php include 'footer.php'; ?>
+			
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+	</body>
+</html>
