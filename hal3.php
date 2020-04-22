2afc4d8bd7e5aa417853a05246d4f1d670c22904
Fernando <>
2020-04-22 21:22:52 +0700

    Add hal3.php

---
+++ hal3.php
@@ -0,0 +1,115 @@
+
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Pola Makan Yang Baik</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+  </head>
+
+  <body>
+
+    <?php include 'navbar.php'; ?>
+
+      <h1>Pola Makan Yang Baik</h1>
+      <hr class="featurette-divider">
+      <div class="container marketing">
+        <div class="row">
+
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="img/pentingsarapan.jpeg" alt="Generic placeholder image" width="140" height="140">
+            <h2>Makan Pagi</h2>
+            <p class="text-justify">Beberapa orang mungkin berpikir bahwa melewatkan sarapan pagi akan membantu program penurunan berat badan. Ada juga yang beranggapan bahwa sarapan pagi akan memberikan efek mengantuk di pagi hari sehingga mereka memilih untuk menghindarinya agar aktivitas paginya tidak terganggu. Padahal anggapan-anggapan tersebut tidak sepenuhnya...</p>
+            <p><a class="btn btn-secondary" href="https://www.lemonilo.com/blog/mengenal-pentingnya-menu-sarapan-pagi-yang-sehat" role="button">View details &raquo;</a></p>
+          </div>
+
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="img/pentingmakansiang.jpg" alt="Generic placeholder image" width="140" height="140">
+            <h2>Makan Siang</h2>
+            <p class="text-justify">Jam makan siang merupakan jam yang ditunggu-tunggu biasanya oleh para karyawan di kantor. Bahkan terkadang kita sudah memikirkannya sejak pagi hari, akan makan dimana kita siang ini? Menu makan siang hari ini apa? Atau makan siang kali ini bersama siapa saja? Tetapi banyak hal yang terjadi setelah makan siang, misalnya wewaktu kembali ke kantor jadi merasa ngantuk karena hidangan...</p>
+            <p><a class="btn btn-secondary" href="https://aura.tabloidbintang.com/cantik-sehat/read/24854/tips-makan-siang-sehat-dan-bermanfaat" role="button">View details &raquo;</a></p>
+          </div>
+
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="img/pentingmakanmalam.jpg" alt="Generic placeholder image" width="140" height="140">
+            <h2>Makan Malam</h2>
+            <p class="text-justify">Makan malam seringkali menjadi waktu yang kita nantikan. Makan dengan santai dan hangat dengan keluarga atau orang tersayang, atau kumpul bareng teman. Namun jangan sembarang pilih menu makan malam. Makan malam menjadi sangat penting karena mampu mengumpulkan anggota keluarga dan berbagi cerita tentang semua aktivitas keseharian. Bahkan menurut...</p>
+            <p><a class="btn btn-secondary" href="http://doktersehat.com/tips-makan-malam-sehat-bersama-keluarga/" role="button">View details &raquo;</a></p>
+          </div>
+
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7">
+            <h2 class="featurette-heading">Khasiat dan Manfaat Sayuran Bagi Tubuh dan Kesehatan</h2>
+            <h4 class="featurette-heading"><span class="text-muted">1. Mencegah dan Mengurangi Stress Berlebih</span></h4>
+            <p class="lead">Dengan kandungan nutrisinya, Sayuran dapat menstimulasi berbagai jenis hormon dan kelenjar di dalam tubuh kita sehingga merangsang mood anda untuk selalu merasa senang dan bahagia.Sayuran dan buah-buahan yang dapat mencegah stres antara lain; ubi, sayurah hijau, tomat, cer, labu, jamur, paprika, buncis, kacang merah, bit, pisang dll.</p>
+            <h4 class="featurette-heading"><span class="text-muted">2. Memperlancar Buang Air Besar</span></h4>
+            <p class="lead">Buang air besar atau BAB adalah proses pembuangan sisa-sisa makanan yang telah diserap tubuh, namun adakalanya proses ini terhambat karena faktor gaya hidup dan sebagainya. Untuk mengatasi masalah seperti ini sayuran dan buah-buahan berguna untuk memberikan asupan serat pada organ pencernaan kita.Sayuran dan buah-buahan yang memiliki kandungan serat tinggi antara lain; bayam, kubis, wortel, pepaya, kacang-kacangan dll.</p>
+            <h4 class="featurette-heading"><span class="text-muted">3. Kaya Kalsium untuk Kesehatan Tulang dan Gigi</span></h4>
+            <p class="lead">Tidak hanya didapatkan dari sumber makanan hewani. Ternyata sayur-sayuran juga mengandung kalsium yang melimpah. Contohnya pada bayam ditemukan sebanyak 300 mg/100 gramnya. Dan kalsium tertinggi ditemukan pada daun kelor mencapai 450 mg.</p>
+            <h4 class="featurette-heading"><span class="text-muted">4. Sayur-Sayuran Mengandung Protein</span></h4>
+            <p class="lead">Bukan rahasia lagi bahwa protein yang berasal dari sayur-sayuran atau yang biasa disebut protein nabati ternyata begitu pentingnya untuk manusia. Dalam rangka membentuk otot dan membangun struktur jaringan sel agar lebih kuat.</p>
+            <h4 class="featurette-heading"><span class="text-muted">5. Kadar Fosfor Cukup Tinggi</span></h4>
+            <p class="lead">Sama dengan kalsium. Fosfor juga berfungsi untuk mencegah tulang dan gigi keropos. Sayuran seperti Bayam Gangeticus ternyata mengandung fosfor teramat tinggi hingga 500 mg/gr.</p>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" src="img/manfaat sayur.jpg" alt="Generic placeholder image">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7 order-md-2">
+            <h2 class="featurette-heading">Manfaat Minum Susu Sapi Setiap Hari Bagi Tubuh</h2>
+            <h4 class="featurette-heading"><span class="text-muted">1. Menjaga Kesehatan Rambut</span></h4>
+            <p class="lead">Semua orang pasti tidak menginginkan mengalami masalah pada rambut, seperti misalnya rambut mengalami kerontokan. Dengan rutin mengkonsumsi susu sapi sudah bisa menjaga kesehatan rambut dan terhindar dari masalah rambut rontok. Ini karena adanya kandungan Vitamin yang baik untuk rambut yaitu Vitamin A dan Vitamin B6. Ditambah lagi dengan adanya kalium pada kandungan susu sapi, tentu ini akan membuat kondisi kesehatan rambut terus terjaga.</p>
+            <h4 class="featurette-heading"><span class="text-muted">2. Menjadi Sumber Energi Bagi Tubuh</span></h4>
+            <p class="lead">Saat menjalani aktifitas sehari-hari tentu saja tubuh harus dalam kondisi yang bagus. Rutin meminum susu setiap hari, apalagi di pagi hari akan membuat tubuh menjadi berenergi dan semangat dalam menjalani aktivitas. Ini karena adanya kandungan kalori pada susu sapi yang bisa sebagai sumber energi bagi tubuh.</p>
+            <h4 class="featurette-heading"><span class="text-muted">3. Menjaga Kesehatan Gigi</span></h4>
+            <p class="lead">Kandungan kalsium pada satu gelas susu mampu menjaga gigi kita agar tetap sehat. Apalagi bila kita meminumnya setiap hari, pastinya kebutuhan kalsium kita akan terpenuhi dan terhindar dari masalah-masalah pada gigi yang terancam menyerang.</p>
+            <h4 class="featurette-heading"><span class="text-muted">4. Memenuhi Kebutuhan Gizi dalam Tubuh</span></h4>
+            <p class="lead">Memenuhi gizi dalam tubuh memang wajib dilakukan bagi kita untuk menjaga tubuh agar tetap sehat. Apabila biasanya kita memenuhi gizi dengan mengkonsumsi buah dan sayuran, salah satu cara lain adalah dengan rutin meminum susu sapi. Ini karena pada susu sapi terdapat Vitamin A, Vitamin D, Vitamin B2 & B12, Protein, Kalium, Kalsium, serta Fosfor.</p>
+            <h4 class="featurette-heading"><span class="text-muted">5. Menjaga Kesehatan Tulang</span></h4>
+            <p class="lead">Kesehatan tulang sangat penting untuk dijaga, apalagi untuk kita yang masih berusia muda. Tentu saja tidak menginginkan terserang masalah pada tulang seperti tulang rapuh / osteoporosis. Susu sapi mampu menjaga kesehatan tulang karena terdapat kandungan kalsium, fosfor, dan magnesium didalamnya.</p>
+          </div>
+          <div class="col-md-5 order-md-1">
+            <img class="featurette-image img-fluid mx-auto" src="img/manfaatsususapi.jpg" alt="Generic placeholder image">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-7">
+              <h2 class="featurette-heading">Manfaat Makan Buah Setiap Hari</h2>
+              <h4 class="featurette-heading"><span class="text-muted">1. Terhindar dari Kanker</span></h4>
+              <p class="lead">Organisasi Kesehatan Dunia menjelaskan, manusia perlu makan lima porsi buah-buahan untuk menjaga diri dari serangan kanker. Sebab, telah ditemukan bahwa sebagian besar kanker terjadi akibat kekurangan nutrisi tertentu. Buah beri dan tomat adalah beberapa buah penting yang harus Anda makan untuk melawan kanker.</p>
+              <h4 class="featurette-heading"><span class="text-muted">2. Mencegah Penyakit Jantung</span></h4>
+              <p class="lead">Salah satu alasan bagi Anda untuk makan buah-buahan setiap hari adalah mencegah penyakit jantung kronis. Buah-buahan yang rendah kalori merupakan sumber vitamin dan mineral. Lagipula, buah-buahan mengandung anti oksidan dan folat yang membantu menurunkan tekanan darah. Apel, jeruk, dan pisang merupakan buah super yang membuat jantung Anda selalu sehat.</p>
+              <h4 class="featurette-heading"><span class="text-muted">3. Memori Semakin Kuat</span></h4>
+              <p class="lead">Dalam sebuah penelitian, ditemukan bahwa makan blueberry dapat meningkatkan memori. Blueberry memiliki nutrisi yang dapat mengembangkan sel-sel otak baru untuk menghentikan kehilangan memori seperti pikun, alzhaimer, dan kehilangan memori jangka pendek.</p>
+              <h4 class="featurette-heading"><span class="text-muted">4. Kulit Bersinar</span></h4>
+              <p class="lead">Apakah Anda ingin kulit bersinar? Resepnya sangat mudah. Anda hanya perlu makan buah setiap hari. Sebagian besar buah-buahan, terutama alpukat, memiliki sejumlah besar vitamin E yang memperlambat penuaan. Sertakan porsi buah-buahan dalam diet Anda sehingga membuat kulit Anda lebih bersinar dan awet muda.</p>
+              <h4 class="featurette-heading"><span class="text-muted">5. Meningkatkan Kekebalan Tubuh</span></h4>
+              <p class="lead">Buah-buahan tertentu memiliki kombinasi besar antara vitamin C, vitamin A, dan berbagai jenis karotenoid, yang meningkatkan daya kekebalan tubuh. Buah-buahan seperti mangga adalah contoh terbaik dalam hal ini. Apabila kekebalan tubuh Anda lemah, mulai sekarang jadikan buah-buahan sebagai makanan yang wajib disantap setiap hari.</p>
+            </div>
+            <div class="col-md-5">
+              <img class="featurette-image img-fluid mx-auto" src="img/manfaatbuah.jpg" alt="Generic placeholder image">
+            </div>
+          </div>
+
+        <hr class="featurette-divider">
+
+      </div>
+
+      <?php include 'footer.php'; ?>
+    
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+  </body>
+</html>
