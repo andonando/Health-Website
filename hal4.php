7a6388763c98c7f6fd52f79598e2443ab4db06f3
Fernando <>
2020-04-22 21:23:28 +0700

    Add hal4.php

---
+++ hal4.php
@@ -0,0 +1,74 @@
+
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Kesehatan Rumah</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+  </head>
+
+  <body>
+      <?php include 'navbar.php'; ?>
+
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+      <div class="col-md-5 p-lg-2 mx-auto my-5">
+        <h1 class="display-4 font-weight-normal">Kesehatan Rumah</h1>
+        <p class="lead font-weight-normal">Kesehatan rumah merupakan cerminan diri kita apakah kita hidup sehat atau tidak. Berikut adalah contoh rumah dalam keadaan sehat dan terawat.</p>
+      </div>
+      <div class="product-device box-shadow d-none d-md-block"></div>
+      <div class="product-device product-device-2 box-shadow d-none d-md-block"></div>
+    </div>
+
+    <div class="d-flex flex-row flex-wrap">
+
+      <div class="d-flex flex-column mx-auto my-3 text-light" style="width: 600px; min-width: 600px; height: 400px; background-image: url('img/rumah sehat1.jpg'); background-repeat: no-repeat; background-size: cover">
+        <div class="text-center mt-auto" style="background-color: rgba(0,0,0,0.5)">
+          <h2>Memiliki Taman</h2>
+          <p>Memiliki tanaman di rumah merupakan contoh rumah yang sehat</p>
+        </div>
+      </div>
+
+      <div class="d-flex flex-column mx-auto my-3 text-light" style="width: 600px; min-width: 600px; height: 400px; background-image: url('img/rumah sehat2.jpg'); background-repeat: no-repeat; background-size: cover">
+        <div class="text-center mt-auto" style="background-color: rgba(0,0,0,0.5)">
+          <h2>Ruang Tamu Memiliki Jendela</h2>
+          <p>Supaya terdapat sirkulasi udara luar dengan udara dalam</p>
+        </div>
+      </div>
+
+      <div class="d-flex flex-column mx-auto my-3 text-light" style="width: 600px; min-width: 600px; height: 400px; background-image: url('img/rumah sehat5.jpg'); background-repeat: no-repeat; background-size: cover">
+        <div class="text-center mt-auto" style="background-color: rgba(0,0,0,0.5)">
+          <h2>Kamar yang Memiliki Jendela</h2>
+          <p>Supaya terdapat sirkulasi udara luar dengan udara dalam</p>
+        </div>
+      </div>
+
+      <div class="d-flex flex-column mx-auto my-3 text-light" style="width: 600px; min-width: 600px; height: 400px; background-image: url('img/rumah sehat3.jpg'); background-repeat: no-repeat; background-size: cover">
+        <div class="text-center mt-auto" style="background-color: rgba(0,0,0,0.5)">
+          <h2>Kamar Mandi dalam Keadaan Kering</h2>
+          <p>Supaya tidak terdapat kuman yang menempel pada lantai</p>
+        </div>
+      </div>
+
+      <div class="d-flex flex-column mx-auto my-3 text-light" style="width: 600px; min-width: 600px; height: 400px; background-image: url('img/rumah sehat4.jpg'); background-repeat: no-repeat; background-size: cover">
+        <div class="text-center mt-auto" style="background-color: rgba(0,0,0,0.5)">
+          <h2>Selalu Bersihkan Kamar</h2>
+          <p>Kamar merupakan bagian yang kita tempati ketika beristirahat malam. Jadi harus dijaga kebersihannya</p>
+        </div>
+      </div>
+
+      <div class="d-flex flex-column mx-auto my-3 text-light" style="width: 600px; min-width: 600px; height: 400px; background-image: url('img/rumah sehat6.jpg'); background-repeat: no-repeat; background-size: cover">
+        <div class="text-center mt-auto" style="background-color: rgba(0,0,0,0.5)">
+          <h2>Selalu Bersihkan Ruang Tengah</h2>
+          <p>Ruang tengah merupakan tempat beristirahat sejenak, karena itu harus selalu dijaga kebersihannya. Jika bisa selalu bersihkan bersama keluarga karena akan mengakrabankan antar anggota keluarga.</p>
+        </div>
+      </div>
+
+    </div>
+
+    <?php include 'footer.php'; ?>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+  </body>
+</html>
