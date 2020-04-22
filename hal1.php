31ac4351042ad0b740c0efa6b2d4c38e32afaf75
Fernando <>
2020-04-22 21:21:25 +0700

    Add hal1php

---
+++ hal1.php
@@ -0,0 +1,124 @@
+
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Menjaga Kesehatan</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <style>
+      .samain{
+        object-fit: cover;
+        object-position: center;
+        width: 320px;
+        height: 240px;
+      }
+    </style>
+  </head>
+
+  <body>
+
+      <?php include 'navbar.php'; ?>
+
+      <section class="jumbotron text-center">
+        <div class="container">
+          <h1 class="jumbotron-heading">Menjaga Kesehatan</h1>
+          <p class="lead text-muted">Banyak cara dalam menjaga kesehtan, berikut caara menjaga kesehatan versi kami.</p>
+        </div>
+      </section>
+
+      <div class="album py-5 px-5 bg-light">
+        <div class="container">
+          <div class="row">
+            
+          <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="samain card-img-top mx-auto" src="img/olahraga.jpg" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text text-justify">Olahraga memperkuat otot inti, mengurangi tekanan dari punggung bawah, juga membangun otot punggung sehingga bisa menopang tulang belakang dengan lebih baik.</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                  <img class="samain card-img-top mx-auto" src="img/toilet.jpg" alt="Card image cap">
+                  <div class="card-body">
+                    <p class="card-text text-justify">Buang air besar secara rutin penting untuk mengatur fungsi usus, selain membantu untuk mengatasi sembelit.</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-4">
+                  <div class="card mb-4 box-shadow">
+                    <img class="samain card-img-top mx-auto" src="img/sikat gigi.jpg" alt="Card image cap">
+                    <div class="card-body">
+                      <p class="card-text text-justify">Bersihkan mulut dari bakteri untuk menghindari penyakit pada gusi. pergilah ke dokter gigi setidaknya sekali setahun. Sikat gigi dengan rajin dan gunakan benang gigi untuk membersihkannya.</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                      <img class="samain card-img-top mx-auto" src="img/puasa.jpg" alt="Card image cap">
+                      <div class="card-body">
+                        <p class="card-text text-justify">Dengan berpuasa organ alat pencernaan kita akan beristirahat, tidak hanya tubuh kita yang perlu istirahat alat pencernaan kita juga perlu istirahat. Selain itu dengan berpuasa juga dapat mengeluarkan racun yang ada di dalam tubuh, proses ini dalam dunia medis dikenal dengan detoksifikasi.</p>
+                      </div>
+                    </div>
+                  </div>
+
+      <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="samain card-img-top mx-auto" src="img/multivitamin.jpg" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text text-justify">Multivitamin diperlukan tubuh anda untuk menambah asupan gizi dari sayuran dan buah-buahan. Minumlah multivitamin yang cocok dengan kesehatan tubuh anda. Multivitamin banyak disediakan di apotik-apotik sehingga anda dengan mudah untuk mendapatkannya. Minumlah secara teratur agar tubuh anda tetap sehat.</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                  <img class="samain card-img-top mx-auto" src="img/air putih.jpg" alt="Card image cap">
+                  <div class="card-body">
+                    <p class="card-text text-justify">Minum air putih sebelum tidur. Air putih akan membantu tubuh anda untuk mengeluarkan racun-racun yang ada di dalam tubuh anda. Saat bangun tidur jangan minum air lain kecuali air putih. Minum air putih ketika bangun tidur baik untuk menyeimbangkan kinerja otak anda yang baru saja diitirahatkan dan akan dipakai kembali.</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-4">
+                  <div class="card mb-4 box-shadow">
+                    <img class="samain card-img-top mx-auto" src="img/kurangi kopi.jpg" alt="Card image cap">
+                    <div class="card-body">
+                      <p class="card-text text-justify">Kurangin mengkonsumsi kopi. Kopi mengandung bahaya kafein yang jika dikonsumsi dalam kadar yang banyak akan menimbulkan efek negatif pada kesehatan tubuh. Kandungan kafein yang tinggi di dalam kopi dapat memperlambat kinerja jantung.</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                      <img class="samain card-img-top mx-auto" src="img/kurangi begadang.jpg" alt="Card image cap">
+                      <div class="card-body">
+                        <p class="card-text text-justify">Bagi yang melakukan banyak aktivitas di siang hari maka hindarilah begadang pada malam harinya. Begadang akan membuat kondisi tubuh menjadi kurang sehat karena angin malam tidak bagus untuk tubuh.</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="col-md-4">
+                      <div class="card mb-4 box-shadow">
+                        <img class="samain card-img-top mx-auto" src="img/kebersihan makanan.jpg" alt="Card image cap">
+                        <div class="card-body">
+                          <p class="card-text text-justify">Semua makanan yang anda konsumsi harus dipastikan kebersihannya. Makanan seperti buah dan sayur cucilah terlebih dahulu sebelum anda mengkonsumsinya. Jika memasak makanan, masaklah sampai matang yang sempurna.</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    </div>
+                    </div>
+        </div>
+
+    <?php include 'footer.php'; ?>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+  </body>
+</html>
