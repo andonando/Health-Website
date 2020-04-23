0fc45b7b901877bc4c881eac8fb871047a0474bc
Fernando <>
2020-04-23 21:12:06 +0700

    Delete some profile

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <footer class="bg-dark text-light text-center mt-5">
 			<div class="d-flex flex-column">
 				<div class="d-flex flex-row">
-					<div class="px-2"><a href="https://id-id.facebook.com/fernando.hasudunganmanik"><img src="img/facebook.png" style="width: 20px; height: 20px;"></div></a>
-					<div class="px-2"><a href="http://www.twitter.com/andonando"><img src="img/twitter.png" style="width: 20px; height: 20px;"></div></a>
-					<div class="px-2"><a href="http://www.instagram.com/andonandoo"><img src="img/instagram.jpg" style="width: 20px; height: 20px;"></div></a>
+					<div class="px-2"><a href="https://id-id.facebook.com/user"><img src="img/facebook.png" style="width: 20px; height: 20px;"></div></a>
+					<div class="px-2"><a href="http://www.twitter.com/user"><img src="img/twitter.png" style="width: 20px; height: 20px;"></div></a>
+					<div class="px-2"><a href="http://www.instagram.com/user"><img src="img/instagram.jpg" style="width: 20px; height: 20px;"></div></a>
                 </div>
 				<hr style="border: 1px solid black; width: 400px">
                 <pclass="float-center">Fernando &copy; 2018</p>
